97675b28ae5ddad07e3c641ec364c4f642f6c786
Dante Barbieri <dantevbarbieri@gmail.com>
2018-05-07 14:17:46 -0500

    Add core gameplay

--- sketch.js
+++ sketch.js
@@ -1,7 +1,103 @@
+const num_rows = 16;
+const num_cols = 30;
+const num_mines = 99;
+let cellSize;
+let board;
+let generated;
+let gameover;
+
 function setup() {
+  window.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+  });
   createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER);
+  cellSize = createVector(width / num_cols, height / num_rows);
+  textSize(min(cellSize.x, cellSize.y));
+  initialize();
+  generated = false;
+  gameover = false;
 }
 
 function draw() {
+  if (gameover) {
+    noLoop();
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j].revealed = true;
+      }
+    }
+  }
   background(0);
+  for (let col of board) {
+    for (let tile of col) {
+      tile.show();
+    }
+  }
+}
+
+function mousePressed() {
+  let r = -1;
+  let c = -1;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j].clicked()) {
+        r = i;
+        c = j;
+      }
+    }
+  }
+  if (!generated) {
+    do {
+      initialize();
+      generate();
+    } while (board[r][c].neighborCount != 0);
+    generated = true;
+  }
+  if (mouseButton == LEFT) {
+    if (board[r][c].revealed && board[r][c].flagCount == board[r][c].neighborCount) {
+      board[r][c].autoFill();
+    }
+    if (!board[r][c].f) {
+      board[r][c].reveal();
+    }
+  } else if (mouseButton == RIGHT) {
+    if (!board[r][c].revealed) {
+      board[r][c].f = !board[r][c].f;
+      for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+          board[i][j].countFlags();
+        }
+      }
+    }
+  }
+
+}
+
+function generate() {
+  for (let i = num_mines; i > 0; i--) {
+    let r = int(random(board.length));
+    let c = int(random(board[0].length));
+    if (board[r][c].b) {
+      i++;
+    } else {
+      board[r][c].b = true;
+    }
+  }
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j].countNeighbors();
+      // board[i][j].revealed = true;
+    }
+  }
+}
+
+function initialize() {
+  board = new Array(num_rows);
+  for (let i = 0; i < board.length; i++) {
+    board[i] = new Array(num_cols);
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j] = new Tile(j * cellSize.x, i * cellSize.y, i, j, cellSize.x, cellSize.y);
+    }
+  }
 }
---
+++ tile.js
@@ -0,0 +1,148 @@
+class Tile {
+
+  constructor(x, y, r, c, w, h) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.c = c;
+    this.w = w;
+    this.h = h;
+    this.b = false;
+    this.f = false;
+    this.revealed = false;
+    this.flagCount = 0;
+    this.neighborCount = 0;
+  }
+
+  show() {
+    stroke(229);
+    this.revealed ? fill(0) : fill(45);
+    rect(this.x, this.y, this.w, this.h);
+    if (this.revealed) {
+      switch (this.neighborCount) {
+        case -1:
+          fill(255, 0, 0);
+          ellipse(this.x + this.w / 2, this.y + this.h / 2, this.w / 2, this.h / 2);
+          break;
+        case 0:
+          break;
+        case 1:
+          fill(0, 0, 200);
+          break;
+        case 2:
+          fill(0, 200, 200);
+          break;
+        case 3:
+          fill(0, 200, 0);
+          break;
+        case 4:
+          fill(200, 200, 0);
+          break;
+        case 5:
+          fill(200, 0, 0);
+          break;
+        case 6:
+          fill(200, 0, 200);
+          break;
+        case 7:
+          fill(200, 100, 200);
+          break;
+        case 8:
+          fill(100, 200, 100);
+          break;
+        default:
+          break;
+      }
+      if (this.neighborCount > 0) {
+        text(this.neighborCount, this.x + this.w / 2, this.y + 7 * this.h / 8);
+      }
+    } else if (this.f) {
+      fill(200, 75, 0);
+      triangle(this.x, this.y, this.x, this.y + this.h, this.x + sqrt(3) * this.w / 2, this.y + this.h / 2);
+    }
+  }
+
+  clicked() {
+    return this.x < mouseX && mouseX < this.x + this.w && this.y < mouseY && mouseY < this.y + this.h;
+  }
+
+  countNeighbors() {
+    if (this.b) {
+      this.neighborCount = -1;
+      return;
+    }
+    let total = 0;
+    for (let xoff = -1; xoff <= 1; xoff++) {
+      for (let yoff = -1; yoff <= 1; yoff++) {
+        let c = this.c + xoff;
+        let r = this.r + yoff;
+        if (c > -1 && c < board[0].length && r > -1 && r < board.length) {
+          let neighbor = board[r][c];
+          if (neighbor.b) {
+            total++;
+          }
+        }
+      }
+    }
+    this.neighborCount = total;
+  }
+
+  countFlags() {
+    let total = 0;
+    for (let xoff = -1; xoff <= 1; xoff++) {
+      for (let yoff = -1; yoff <= 1; yoff++) {
+        let c = this.c + xoff;
+        let r = this.r + yoff;
+        if (c > -1 && c < board[0].length && r > -1 && r < board.length) {
+          let neighbor = board[r][c];
+          if (neighbor.f) {
+            total++;
+          }
+        }
+      }
+    }
+    this.flagCount = total;
+  }
+
+  reveal() {
+    if (this.b) {
+      gameover = true;
+    }
+    this.revealed = true;
+    this.f = false;
+    if (this.neighborCount == 0) {
+      this.floodFill();
+    }
+  }
+
+  floodFill() {
+    for (let xoff = -1; xoff <= 1; xoff++) {
+      for (let yoff = -1; yoff <= 1; yoff++) {
+        let c = this.c + xoff;
+        let r = this.r + yoff;
+        if (c > -1 && c < board[0].length && r > -1 && r < board.length) {
+          let neighbor = board[r][c];
+          if (!neighbor.b && !neighbor.revealed) {
+            neighbor.reveal();
+          }
+        }
+      }
+    }
+  }
+
+  autoFill() {
+    for (let xoff = -1; xoff <= 1; xoff++) {
+      for (let yoff = -1; yoff <= 1; yoff++) {
+        let c = this.c + xoff;
+        let r = this.r + yoff;
+        if (c > -1 && c < board[0].length && r > -1 && r < board.length) {
+          let neighbor = board[r][c];
+          if (!neighbor.f && !neighbor.revealed) {
+            neighbor.reveal();
+          }
+        }
+      }
+    }
+  }
+
+}
